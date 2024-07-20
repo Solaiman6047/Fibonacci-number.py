a122f87c7835b75017df5730b0d942ae96566c57
Hana <hanasolaiman848@gmail.com>
2024-07-20 19:19:51 +0300

    che

--- task5.1.7.py
+++ task5.1.7.py
@@ -2,7 +2,7 @@ lettre=input("Enter the column's lettre: ")
 number=int(input("Enter the row's number: "))
 column=["a","b","c","d","e","f","g","h"]
 
-if lettre in column and number < 8 and number > 1:
+if lettre in column and number <= 8 and number >= 1:
      let=ord(lettre)
      res=let+number
      res%=2
