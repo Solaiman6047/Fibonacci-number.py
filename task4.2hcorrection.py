e5e8ee8af48769a8213722995a9019589b9d702a
Hana <hanasolaiman848@gmail.com>
2024-07-21 14:39:26 +0300

    calculator

--- task4.2hcorrection.py
+++ task4.2hcorrection.py
@@ -2,15 +2,35 @@ number1=int(input("enter first number:"))
 operator=input("enter operator:")
 number2=int(input("enter second number:"))
 
+def add(number1,number2):
+    return number1+number2
+    
+def sub(number1,number2):
+    return number1-number2
+    
+def mult(number1,number2):
+    return number1*number2
+    
+def div(number1,number2):
+    if number2==0:
+        return "undefined answer"
+    else:
+        return number1/number2
+
+def mod(number1,number2):
+    return number1%number2
+
+
 if operator=="+":
-    print(f"answer=",number1+number2)
+    z=sum(number1,number2)
 elif operator=="-":
-    print(f"answer=",number1-number2)
+    z=sub(number1,number2)
 elif operator=="*":
-    print(f"answer=",number1*number2)
-elif operator=="/" and number2!=0:
-    print(f"answer=",number1/number2)
+    z=mult(number1,number2)
+elif operator=="/":
+    z=div(number1,number2)
 elif operator=="%":
-    print(f"answer=",number1%number2)
+    z=mod(number1,number2)
 else:
-    print("undefined number")
+    print("undefined value")
+print(z)
